25172f2fe3bbdd37e1c8133ee7214d509e4544fd
AmandaSchererMeirelles <mandy.floripa2015@hotmail.com>
2023-11-01 21:32:18 -0300

    controllers

--- api/src/models/controllers/ClienteController.ts
+++ api/src/models/controllers/ClienteController.ts
@@ -5,7 +5,7 @@ import { ILike } from "typeorm";
 export class ClienteController {
 
     async list (req: Request, res: Response): Promise<Response> {
-        let categoria: Cliente[] = await Cliente.find();
+        let cliente: Cliente[] = await Cliente.find();
 
         return res.status(200).json(Cliente);
     }
@@ -13,33 +13,33 @@ export class ClienteController {
     async create (req: Request, res: Response): Promise<Response> {
         let body = req.body;
        
-        let categoria: Cliente = await Cliente.create({
-            descricao: body.descricao,
+        let cliente: Cliente = await Cliente.create({
+            nome: body.nome,
         }).save();
     
         return res.status(200).json(Cliente);
     }
 
     async delete (req: Request, res: Response): Promise<Response> {
-        let categoria: Cliente = res.locals.cliente;
+        let cliente: Cliente = res.locals.cliente;
     
-        categoria.remove();
+        cliente.remove();
         
         return res.status(200).json();
     }
 
     async find (req: Request, res: Response): Promise<Response> {
-        let categoria: Cliente = res.locals.cliente;
+        let cliente: Cliente = res.locals.cliente;
   
         return res.status(200).json(Cliente);
     }
 
     async update (req: Request, res: Response): Promise<Response> {
         let body = req.body;
-        let categoria: Cliente = res.locals.cliente;
+        let cliente: Cliente = res.locals.cliente;
     
-        Cliente.descricao = body.descricao,
-        await categoria.save();
+        Cliente.nome = body.nome,
+        await nome.save();
     
         return res.status(200).json(Cliente);
     } 
